4256ccbc257329224bd5804b5cb79fd3decac911
Павел Филипенко <zeptor-online@yandex.ru>
2022-06-21 08:28:51 +0700

    Первый шаблон проекта

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <title>DevLife - Мысли просто</title>
-    <meta name="description" content="DevLife - Быстрая и качественная разработка сайтов под ключ, а также логотипов, визиток и Landing-page"/>
-    <link rel="icon" href="https://devlife.ru/favicon.svg" type="image/x-icon">
+    <title>DevLife - Профессиональная разработка web проектов</title>
+    <meta name="description" content="Студия DevLife занимается созданием, разработкой и сопровождением интернет проектов. Мы всегда нацелены на качество и открытость в работе с заказчиками. Будем рады видеть вас в роли наших клиентов."/>
+    <link rel="manifest" href="/manifest.json">
+    <link rel="icon" href="https://devlife.ru/favicon.ico" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+   <!-- <script src="https://use.fontawesome.com/0633dfeedc.js"></script> -->
+    <!-- Renew version CSS file -->
     <script>
         let epochId = Date.now();
         (function () {
@@ -19,12 +21,12 @@
             document.head.appendChild(mainccs);
         }())
     </script>
+    <!-- Renew version CSS file -->
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
             m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
         ym(85955552, "init", {
             clickmap:true,
             trackLinks:true,
@@ -33,56 +35,185 @@
     </script>
     <noscript><div><img src="https://mc.yandex.ru/watch/85955552" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
+
     <!-- <link rel="stylesheet" href="css/main.css"> -->
 </head>
 <body>
     <header class="header">
         <div class="container">
-            <div class="head">
-                <div class="logo">
-                    <a href="/"><img src="images/logo.png" alt="logo"></a>
+            <div class="header_wrap">
+                <div class="head">
+                    <div class="logo">
+                        <a href="/">
+                            <img src="images/logo.png" alt="logo">
+                        </a>
+                    </div>
+                    <div class="title_menu">
+                        <nav class="menu">
+                            <ul>
+                                <li><a href="#">Главная</a></li>
+                                <li><a href="#">Услуги</a></li>
+                                <li><a href="#">Преимущества</a></li>
+                                <li><a href="#">Эксклюзив</a></li>
+                                <li><a href="#">Примеры</a></li>
+                                <li><a href="#">Отзывы</a></li>
+                                <li><a href="#">Бриф</a></li>
+                                <li><a href="#">Заказ</a></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-                <div class="main_menu">
-                    <nav class="menu">
-                        <ul>
-                            <li><a href="#">Главная</a></li>
-                            <li><a href="#">Команда</a></li>
-                            <li><a href="#">Примеры работ</a></li>
-                            <li><a href="#">Отзывы</a></li>
-                            <li><a href="#" class="contact">Связаться с нами</a></li>
-                        </ul>
-                    </nav>
+                <div class="first_screen">
+                    <h1 class="site_name">DevLife</h1>
+                    <h2 class="site_concept">Разработка сайтов под ключ</h2>
+                    <hr class="line">
+                    <div class="description">
+                        Студия "DevLife" занимается созданием, разработкой и сопровождением интернет проектов. Мы всегда нацелены на качество и открытость в работе с заказчиками. Будем рады видеть вас в роли наших клиентов.
+                    </div>
+                    <a href="#" class="next">Перейти к услугам</a>
                 </div>
             </div>
         </div>
     </header>
-    <section class="leader">
-        <div class="wrap">
-            <div class="container">
-                <div class="leader_head">
-                    <div class="leader_one animate__animated animate__backInLeft">
-                        <h3 class="full">
-                            Full stack
-                        </h3>
-                        <h2 class="dev">
-                            Разработка
-                        </h2>
-                        <div class="desc">
-                            <div class="block"></div>
-                            <div class="desc_text">
-                                <p>Услуги по созданию логотипов, визиток, landing-page и прочих сайтов как с нуля, так и на популярных CMS системах.</p>
-                                <div class="cont">
-                                    <a href="#">Заказать работу</a>
-                                </div>
-                            </div>
+    <section class="services">
+        <div class="container">
+            <div class="services_wrap">
+                <h2 class="services_name">Наши услуги</h2>
+                <div class="services_desc">Выберите оптимальное решение для своего будущего или уже существующего проекта.</div>
+                <hr class="min_line">
+                <div class="provide">
+                    <div class="provide_item">
+                        <i class="fa fa-internet-explorer" aria-hidden="true"></i>
+                        <h4 class="provide_name">Одностраничный сайт</h4>
+                        <hr class="min_line">
+                        <div class="provide_desc">
+                            Одностраничный сайт (LandingPage) быстро создается и прост в обслуживании, но при этом передаёт всю важную информацию о вашем бизнесе.
                         </div>
+                        <a href="#">Заказать услугу <i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
                     </div>
-                    <div class="leader_two animate__animated animate__backInRight">
-                        <img src="images/comp.png" alt="comp">
+                    <div class="provide_item">
+                        <i class="fa fa-building" aria-hidden="true"></i>
+                        <h4 class="provide_name">Сайт организации</h4>
+                        <hr class="min_line">
+                        <div class="provide_desc">
+                            Это интернет ресурс, содержащий полную информацию о компании, её услугах/продукции, событиях в жизни фирмы, историю развития и многое другое.
+                        </div>
+                        <a href="#">Заказать услугу <i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="provide_item">
+                        <i class="fa fa-id-card" aria-hidden="true"></i>
+                        <h4 class="provide_name">Сайт визитка</h4>
+                        <hr class="min_line">
+                        <div class="provide_desc">
+                            Представляет собой простой небольшой веб-ресурс, состоящий из одной или нескольких страниц, на которых указана главная информация о компании, продукте, услуге или личности. Это удобный и современный способ рассказать о себе. Компании или личности больше не придется рассказывать о себе каждому заинтересованному человеку. Можно просто перенаправлять на сайт, где уже указана вся важная информация.
+                        </div>
+                        <a href="#">Заказать услугу <i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="provide_item">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                        <h4 class="provide_name">Интернет магазин</h4>
+                        <hr class="min_line">
+                        <div class="provide_desc">
+                            Интерактивный веб-сайт, рекламирующий товар или услугу, принимающий заказы на покупку, предлагающий пользователю выбор варианта расчета, способа получения заказа и выписывающий счет на оплату.
+                        </div>
+                        <a href="#">Заказать услугу <i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="provide_item">
+                        <i class="fa fa-cogs" aria-hidden="true"></i>
+                        <h4 class="provide_name">Доработки по сайту</h4>
+                        <hr class="min_line">
+                        <div class="provide_desc">
+                            Оптимизация функционала: правки меню, доработка формы обратной связи, изменения на страницах. Корректировка ошибок в коде, либо его частичная доработка.
+                        </div>
+                        <a href="#">Заказать услугу <i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
+                    </div>
+                    <div class="provide_item">
+                        <i class="fa fa-laptop" aria-hidden="true"></i>
+                        <h4 class="provide_name">Верстка сайта</h4>
+                        <hr class="min_line">
+                        <div class="provide_desc">
+                            Создание структуры гипертекстового документа на основе HTML-разметки, как правило, при использовании таблиц стилей и клиентских сценариев, таким образом, чтобы элементы дизайна выглядели аналогично макету.
+                        </div>
+                        <a href="#">Заказать услугу <i class="fa fa-hand-o-right" aria-hidden="true"></i></a>
                     </div>
                 </div>
             </div>
         </div>
+    </section>
+    <section class="advantage">
+        <div class="container">
+            <div class="advantage_wrap">
+                <h2 class="services_name white">Преимущества</h2>
+                <div class="services_desc white">Почему из такого обширного количество студий, стоит выбрать нас.</div>
+                <hr class="min_line blue">
+                <div class="advantage_items">
+                    <div class="adv_item">
+                        <i class="fa fa-line-chart" aria-hidden="true"></i>
+                        <h4 class="adv_desc">Качество</h4>
+                        <div class="adv_info">Всегда качественное исполнение и ответственность перед клиентами</div>
+                    </div>
+                    <div class="adv_item">
+                        <i class="fa fa-user-plus" aria-hidden="true"></i>
+                        <h4 class="adv_desc">Гарантия</h4>
+                        <div class="adv_info">Гарантия на поддержку проекта до 3-х месяцев</div>
+                    </div>
+                    <div class="adv_item">
+                        <i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+                        <h4 class="adv_desc">Поддержка</h4>
+                        <div class="adv_info">Мы всегда готовы ответить на ваши вопросы</div>
+                    </div>
+                    <div class="adv_item">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                        <h4 class="adv_desc">Профессионализм</h4>
+                        <div class="adv_info">У нас лучшие исполнители, проверенные временем</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="exclusive">
+        <div class="container">
+            <div class="exclusive_wrap">
+                <h2 class="exc_qwest">У вас индивидуальный проект? Давайте обсудим!</h2>
+                <form action="index.php" class="exc_form" method="post">
+                    <input type="text" name="fio" placeholder="Имя">
+                    <input type="phone" name="phone" placeholder="+7(999)999-99-99">
+                    <button class="send" type="submit">Заказать звонок</button>
+                </form>
+            </div>
+        </div>
+    </section>
+    <section class="cases">
+        <div class="container">
+            <div class="cases_wrap">
+                <h2 class="services_name">Наши проекты</h2>
+                <div class="services_desc">Здесь представлены проекты выпущенные в продакшн нашей студией.</div>
+                <hr class="min_line">
+                <div class="cases_items">
+                    <div class="cases_item_big">
+                        <a href="#"><img src="images/cases.jpg" alt="#"></a>
+                    </div>
+                    <div class="cases_item_wrap">
+                        <div class="cases_item_mini">
+                            <div class="cases_item_lit">
+                                <a href="#"><img src="images/cat.png" alt="#"></a>
+                            </div>
+                            <div class="cases_item_lit">
+                                <a href="#"><img src="images/cat2.png" alt="#"></a>
+                            </div>
+                        </div>
+                        <div class="cases_item_middle">
+                            <a href="#"><img src="images/Cat3.png" alt="#"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="cases">
+        <center>
+            <h1>Тестовый проект. Сайт находится в разработке. Приносим извинения за доставленные неудобства</h1>
+        </center>
     </section>
     <script src="js/script.js"></script>
 </body>
